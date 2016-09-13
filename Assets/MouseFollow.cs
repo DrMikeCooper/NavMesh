7fdaa14e03aae1b81d5ac112f5e0ff8da69a4dee
Dr_Mike_Cooper <coriolisboy@gmail.com>
2016-09-13 17:04:18 +0930

    Navmesh wallrunning demo

---
+++ Assets/MouseFollow.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollow : MonoBehaviour {
+
+    NavMeshAgent nv;
+    MeshRenderer mr;
+    public Material linkMaterial;
+    public Material noLinkMaterial;
+    PointSequence currentPoints = null;
+    int currentIndex = 0;
+
+    // Use this for initialization
+    void Start () {
+        nv = GetComponent<NavMeshAgent>();
+        mr = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (nv.currentOffMeshLinkData.offMeshLink != null)
+        {
+            mr.material = linkMaterial;
+            if (currentPoints == null)
+            {
+                currentPoints = nv.currentOffMeshLinkData.offMeshLink.endTransform.GetComponent<PointSequence>();
+                currentIndex = 0;
+            }
+        }
+        else
+        {
+            mr.material = noLinkMaterial;
+        }
+
+        // move along the list of transforms if we have some
+        if (currentPoints != null)
+        {
+            // at the end, we want to return to the NavMesh
+            Transform from = currentPoints.allPoints[currentIndex];
+            Transform target = currentPoints.allPoints[currentIndex+1];
+
+            Vector3 pos = transform.position;
+            pos = Vector3.MoveTowards(pos, target.position, 0.1f);
+            if ((pos - target.position).magnitude < 0.05f)
+            {
+                if (currentIndex >= currentPoints.allPoints.Length - 2)
+                {
+                    currentPoints = null;
+                    nv.CompleteOffMeshLink();
+                }
+                else
+                {
+                    currentIndex++;
+                }
+            }
+
+            transform.position = pos;
+
+        }
+
+        bool touch = false;
+        Vector3 mousepos = new Vector3(0, 0, 0);
+
+#if MOBILE_INPUT
+        if (Input.touchCount > 0)
+        {
+            touch = true;
+            mousepos = Input.GetTouch(0).position;
+        }
+#else
+        if (Input.GetMouseButton(0))
+        {
+            touch = true;
+            mousepos = Input.mousePosition;
+        }
+#endif
+        if (!touch)
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(mousepos);
+        RaycastHit hitInfo = new RaycastHit();
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            nv.SetDestination(hitInfo.point);
+        }
+
+
+    }
+}
---
+++ Assets/PointSequence.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointSequence : MonoBehaviour {
+
+    public Transform[] points;
+
+    [HideInInspector]
+    public Transform[] allPoints;
+
+	// Use this for initialization
+	void Start () {
+        OffMeshLink link = GetComponent<OffMeshLink>();
+        int num = points.Length;
+        allPoints = new Transform[num + 2];
+        allPoints[0] = link.startTransform;
+        for (int i=0; i<num; i++)
+            allPoints[i+1] = points[i];
+        allPoints[num + 1] = link.endTransform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
